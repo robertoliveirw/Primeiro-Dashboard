e14354e5925c3db2bf20cffa47cbcbeea026841b
Roberto <oliveirarobertw@gmail.com>
2025-01-14 21:36:11 -0300

    grafico de rating

--- dashboard.py
+++ dashboard.py
@@ -30,6 +30,13 @@ col1.plotly_chart(fig_date) # Reservando o espaço col1 para o gráfico
 fig_prod = px.bar(df_filtered, x="Date", y="Product line", color="City", title="Faturamento por tipo de produto", orientation="h") # Gráfico de barra na horizontal
 col2.plotly_chart(fig_prod) # Reservando o espaço col2 para o gráfico
 
-city_total = df_filtered.groupby("City")[["Total"]].sum().reset_index() # Contribuição por cidade
+city_total = df_filtered.groupby("City")[["Total"]].sum().reset_index() # Agrupando a contribuição por filial
 fig_city = px.bar(city_total, x="City", y="Total", title="Faturamento por filial") #Gráfico de barra da contribuição por filial
 col3.plotly_chart(fig_city, use_container_width=True) # Reservando o espaço col3 para o gráfico
+
+fig_kind = px.pie(df_filtered, values="Total", names="Payment", title="Faturamento por tipo de pagamento") #Gráfico de pizza para forma de pagemento
+col4.plotly_chart(fig_kind, use_container_width=True) # Reservando o espaço col4 para o gráfico
+
+city_total = df_filtered.groupby("City")[["Rating"]].mean().reset_index() # Agrupando a rating por filial
+fig_rating = px.bar(df_filtered, y="Rating", x="City", title="Avaliação") # gráfico da rating
+col5.plotly_chart(fig_rating, use_container_width=True) # Reservando o espaço col5 para o gráfico
